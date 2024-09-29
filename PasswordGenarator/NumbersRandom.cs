98c4d59016097d2e50dbceb33df1add7fd823c72
ledosnardo <ledosnardo@gmail.com>
2024-09-29 19:35:08 -0300

    Adicionar arquivos de projeto.

---
+++ PasswordGenarator/NumbersRandom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenarator
+{
+    public class NumbersRandom { 
+        public StringBuilder Randons(int qtd)
+        {
+            string carateres = "abcdefghijklmnopqrstuvwxyz" + 
+                "abcdefghijklmnopqrstuvwxyz".ToUpper() + "1234567890" +
+                "!@#$%&";
+
+            StringBuilder password = new StringBuilder();
+            Random rand = new Random();
+
+            for (int i = 0; i < qtd; i++)
+            {
+                password.Append(carateres[ rand.Next(0, carateres.Length) ]);
+            }
+
+            return password;
+        }
+    }
+}
---
+++ PasswordGenarator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenarator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtd = 10;
+
+            StringBuilder s = new NumbersRandom().Randons(qtd);
+
+            Console.Write(s);
+            Console.ReadKey();
+        }
+    }
+}
